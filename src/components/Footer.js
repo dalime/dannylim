accd596d6ef73e03867773daaa7da35703bc21ce
duhdanube <Danny.B.Lim@gmail.com>
2016-09-18 23:27:12 -0700

    Footer icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,13 +2,14 @@ import React from 'react';
 import FlatButton from 'material-ui/FlatButton';
 import FontIcon from 'material-ui/FontIcon';
 import ActionAndroid from 'material-ui/svg-icons/action/android';
+//import ActionTwitter from 'material-ui/svg-icons/action/twitter';
 import {fullWhite} from 'material-ui/styles/colors';
 
 const style = {
   margin: 12,
 };
 
-const FlatButtonExampleIcon = () => (
+const Footer = () => (
   <div id='connect'>
     <FlatButton
       icon={<ActionAndroid />}
@@ -18,16 +19,16 @@ const FlatButtonExampleIcon = () => (
       href="https://github.com/dalime"
       target="_blank"
       secondary={true}
-      icon={<FontIcon className="muidocs-icon-github" />}
+      icon={<ActionAndroid />}
       style={style}
     />
     <FlatButton
       backgroundColor="#a4c639"
       hoverColor="#8AA62F"
-      icon={<FontIcon className="muidocs-icon-twitter" />}
+      icon={<FontIcon className="muidocs-icon-android" />}
       style={style}
     />
   </div>
 );
 
-export default FlatButtonExampleIcon;
+export default Footer;
