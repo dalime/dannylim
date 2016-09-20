694992704d78f6a46ee201c02be28556211895ab
duhdanube <Danny.B.Lim@gmail.com>
2016-09-20 12:28:23 -0500

    About section skeleton

--- src/components/About.js
+++ src/components/About.js
@@ -8,16 +8,67 @@ export default class App extends Component {
       <div id='about' className='container'>
         <div className="col-sm-12 col-md-12 col-lg-12">
           <div className='row'>
-            <h2 className='col-sm-12 col-md-12 col-lg-12 text-center'>ABOUT</h2>
+            <h2 className='text-center'>ABOUT</h2>
           </div>
           <div className="row">
-            <h5 className='col-sm-12 col-md-10 col-lg-11'>
-              Hi, my name is Danny and I am a Fullstack JavaScript Developer. Born and raised in Maryland between Baltimore and Washington, D.C., I have an aptitude for service.
+            <div className="col-sm-12 col-md-4 col-lg-3">
+              <Avatar size='200px'/>
+            </div>
+            <div className='col-sm-12 col-md-8 col-lg-9'>
+              <h3>Summary</h3>
+              <h5>
+              Hi, my name is Danny and I am a Fullstack JavaScript Developer.
+              Born and raised in Maryland between Baltimore and Washington, D.C., I have an aptitude for service.
               I earned my Bachelor's in Information Systems and Accounting from the Robert H. Smith School of Business at the University of Maryland.
-              I worked in technology risk consulting before switching to the world of development. Currently, I am building apps with JavaScript, particularly the React.js and Redux.js libraries.
+              I worked in technology risk consulting, where I served clients by auditing their information systems and business processes, before switching to the world of development.
+              I decided to move into app development due to my desire to create meaningful solutions.
+              My consulting experience has taught me the value of open communication both internally with a team and externally with clients and has been invaluable in building with teams.
+              Currently, I am building apps with JavaScript, particularly the React.js and Redux.js libraries.
               I am very fond of the MERN Stack (MongoDB, Express.js, React.js, and Node.js) for fullstack app development.
-            </h5>
-            <Avatar size='200px' className='col-sm-12 col-md-2 col-lg-1'/>
+              </h5>
+            </div>
+          </div>
+          <div className="row">
+            <h3>Education</h3>
+          </div>
+          <div className="row">
+            <div className="col-sm-6 col-md-8 col-lg-9">
+              <h4>Robert H. Smith School of Business</h4>
+              <h5>Bachelor of Science in Information Systems and Accounting</h5>
+            </div>
+            <div className="col-sm-6 col-md-4 col-lg-3">
+              <Avatar size='75px'/>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-sm-6 col-md-8 col-lg-9">
+              <h4>Free Code Camp</h4>
+              <h5>Self taught JavaScript development</h5>
+            </div>
+            <div className="col-sm-6 col-md-4 col-lg-3">
+              <Avatar size='75px' />
+            </div>
+          </div>
+          <div className="row">
+            <h3>Experience</h3>
+          </div>
+          <div className="row">
+            <div className="col-sm-6 col-md-8 col-lg-9">
+              <h4>a2z, Inc.</h4>
+              <h5>Software Development Intern</h5>
+            </div>
+            <div className="col-sm-6 col-md-4 col-lg-3">
+              <Avatar size='75px' />
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-sm-6 col-md-8 col-lg-9">
+              <h4>Deloitte</h4>
+              <h5>Advisory Consultant</h5>
+            </div>
+            <div className="col-sm-6 col-md-4 col-lg-3">
+              <Avatar size='75px' />
+            </div>
           </div>
         </div>
       </div>
